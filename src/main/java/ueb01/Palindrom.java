469881e8d6fa83b70bbf43b4320215aca593aaac
bernh <bernhardkohler@gmx.de>
2019-05-06 01:33:01 +0200

    no message

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -1,9 +1,41 @@
 package ueb01;
 
+import java.util.Arrays;
+import java.util.*;
+
 class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
 
-		return false;
+		s = s.replaceAll("\\s", "");
+		s = s.toLowerCase();
+
+		char [] stringArr = s.toCharArray();
+
+			if (stringArr.length <= 1) {
+
+			return true;
+
+			}
+
+			else {
+
+
+				char[] part1 = new char[stringArr.length / 2];
+				char[] part2 = new char[stringArr.length / 2];
+
+				for (int i = 0; i < part1.length; i++) {
+
+					part1 [i] = stringArr[i];
+					part2 [i] = stringArr[stringArr.length - 1 - i];
+
+				}
+
+				return Arrays.equals(part1, part2);
+
+			}
+
+
+
 	}
 }
